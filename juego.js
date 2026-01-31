0088abb9850d92f14921c406e784e7a7f5179449
Zaquimol <brunimolinas@gmail.com>
2026-01-30 21:58:01 -0300

    Se cambio el nombre del div en el js

--- juego.js
+++ juego.js
@@ -8,14 +8,14 @@ class MainScene extends Phaser.Scene {
         this.load.image('logo', 'activos/logo-juego.png');
     }
 
-  
+
 
     // Funcion de cargar initializar
 
     create() {
         // Initializar los objetos de juegos y configuraciones aqui
 
-        this.add.image(400, 300, 'logo'); 
+        this.add.image(400, 300, 'logo');
 
         // Agregar fisicas al jugador (sprite)
         // const jugador = this.physics.add.sprite(100, 100, 'jugador'); 
@@ -24,8 +24,8 @@ class MainScene extends Phaser.Scene {
 
     //Funcion que se repite cada frame
     update() {
-      // Logica del juego, se ejecuta cada frame ( cada actualizacion de images por segundos )
-      // Por ejemplo a 30 fps (frame per seconds)
+        // Logica del juego, se ejecuta cada frame ( cada actualizacion de images por segundos )
+        // Por ejemplo a 30 fps (frame per seconds)
     }
 }
 
@@ -35,7 +35,7 @@ const configuracion = {
     type: Phaser.AUTO, // Use WebGL if available, otherwise Canvas
     width: 800,
     height: 600,
-    parent: 'game-container', // Cambiar al nombre del index.html
+    parent: 'contenedor-juego', // Cambiar al nombre del index.html
     scene: MainScene, // La escena a usar
     physics: {
         default: 'arcade', // Use the Arcade physics engine
